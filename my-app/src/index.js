04cc257c7264f06c68ef96a4df88b769e8fd2c07
Patrick <Patrick.Hwang@infinite.com>
2021-08-08 19:36:18 -0500

    move board to its own file

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,42 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-function Square(props) {
-  return (
-    <button className="square" style={{ backgroundColor: props.bgColor }} onClick={props.onClick}>
-      {null}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.props.board[i]}
-        bgColor={ (this.props.board[i].type !== "empty" ? "blue" : "white") }
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-    let rows = [];
-    for(let i = 0; i < 10; ++i) {
-      let squares = [];
-      for(let j = 0; j < 10; ++j) {
-        squares.push(this.renderSquare(10*i + j));
-      }
-      rows.push(<div className="board-row">{squares}</div>)
-    }
-    return (
-      <div>
-        {rows}
-      </div>
-    );
-  }
-}
+import Board from './components/Board'
 
 class Game extends React.Component {
   constructor(props) {
